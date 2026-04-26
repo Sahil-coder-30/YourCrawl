46f9f344e84e3639247c6b402a711d34e1eaf740
Sahil Sharma <sahilsharma3043@gmail.com>
2026-04-26 12:25:59 +0530

    Delete Backend directory

--- Backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { config } from "./config.js";
-
-// Triggering nodemon restart to load new .env
-export const connectToDb =async ()=>{
-    await mongoose.connect(config.MONGO_URI);
-    console.log("Connected to MongoDB");
-}
